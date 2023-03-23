edee6fb5136681a338a305c11d5a0d0831facc24
milekajtez <milekajtez@gmail.com>
2023-03-23 22:43:42 +0100

    Add raport pages

---
+++ GasOutageReporterService/GasOutageReporter.Api/Pages/Report.cshtml.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GasOutageReporter.Api.Pages
+{
+    public class ReportModel : PageModel
+    {
+        //public string Message { get; set; }
+
+        public void OnGet()
+        {
+            //Message = "Here you can add new report. Also you can add for eact report it's steps and worker.";
+        }
+    }
+}
