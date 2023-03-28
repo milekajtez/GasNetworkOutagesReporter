4eb33527bb5adaf9039e7893fb41d3597ed08522
milekajtez <milekajtez@gmail.com>
2023-03-28 09:24:48 +0200

    Add summary service

---
+++ GasReporterApplication/Common/Helpers/SummaryData.cs
@@ -0,0 +1,11 @@
+﻿using Common.Models;
+using System.Collections.Generic;
+
+namespace Common.Helpers
+{
+    public class SummaryData
+    {
+        public Report Report { get; set; }
+        public List<Step> Steps { get; set; }
+    }
+}
--- GasReporterApplication/GasReporter.Api/Pages/History.cshtml.cs
+++ GasReporterApplication/GasReporter.Api/Pages/History.cshtml.cs
@@ -1,12 +1,36 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Common.Abstractions;
+using Common.Helpers;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Collections.Generic;
+using System.ServiceModel;
 
 namespace GasReporter.Api.Pages
 {
     public class HistoryModel : PageModel
     {
+        public List<SummaryData> ArchivedReports { get; set; }
+
         public void OnGet()
         {
+            var myBinding = new NetTcpBinding(SecurityMode.None);
+            var myEndpoint = new EndpointAddress("net.tcp://localhost:7001/SummaryServiceEndpoint");
+            var validationFactory = new ChannelFactory<ISummary>(myBinding, myEndpoint);
 
+            ISummary summary = null;
+            try
+            {
+                summary = validationFactory.CreateChannel();
+                ArchivedReports = summary.GetSummaryData(true);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                (summary as ICommunicationObject)?.Abort();
+            }
+
+            ((ICommunicationObject)summary).Close();
+            validationFactory.Close();
         }
     }
 }
